8331aa10166d391289002fa3299c0bb28d69870d
JeremyKidd <43313177+JeremyKidd@users.noreply.github.com>
2026-07-08 10:33:07 +0100

    Change file extension from .cpr to .riff

--- src/cpr2ardour/main.py
+++ src/cpr2ardour/main.py
@@ -4,7 +4,7 @@ from cpr2ardour.binary import BinaryReader
 from cpr2ardour.riff import read_riff
 
 
-with BinaryReader.open(Path("tests/data/Sapphic3.cpr")) as reader:
+with BinaryReader.open(Path("tests/data/Sapphic3.riff")) as reader:
     riff = read_riff(reader)
 
 print(riff)
